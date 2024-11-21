697bda9a1075f930992aa50108ee0d42c8d50f76
Adam <s190491@student.pg.edu.pl>
2024-11-21 20:19:32 +0100

    Pierwsze małe analizy

--- ProjektAnalizaDanychV1.R
+++ ProjektAnalizaDanychV1.R
@@ -1,3 +1,19 @@
+library(ggplot2)
 library(readr)
-silownia_new <- read_csv("silownia_new.csv")
-View(silownia_new)
+library(naniar)
+library(visdat)
+silownia <- read_csv("silownia_new.csv")
+
+View(silownia)
+
+
+vis_miss(silownia)
+vis_dat(silownia)
+gg_miss_var(silownia)
+
+#Braki danych występują w kolumnie Workout_Type, Age oraz BMI
+#Kolumna Age oraz BMI posiadają wartości liczbowe, a kolumna Workout_Type to ciąg znaków (wyraz)
+#W Age braki stanowią 10%, w Workout_Type 15%, a w BMI 15 %
+
+kolumny_z_NA <- silownia %>% 
+                  select(Age, Workout_Type, BMI)
